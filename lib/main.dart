7107e6e2b3d0c9cf97d0a9469403810f23625493
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-07-07 22:23:48 +0200

    update: main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_list/bloc/list/list_bloc.dart';
+import 'package:to_do_list/bloc/list/list_state.dart';
+import 'package:to_do_list/page/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ToDoListApp());
+}
+
+class ToDoListApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => ListBloc(UninitializedListState()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+  }
 }
